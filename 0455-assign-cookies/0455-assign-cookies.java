ab61e8c57e32f53b6536aab586a084489eea2f29
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-04-28 21:35:04 +0530

    Time: 15 ms (64.62%), Space: 51.2 MB (92.13%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(s);
+        Arrays.sort(g);
+
+        int i=0,j=0,count=0;
+        while(i<g.length && j<s.length)
+        {
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+                j++;
+            }
+            else j++;  // try next cookie
+        }
+        return count;
+    }
+}
