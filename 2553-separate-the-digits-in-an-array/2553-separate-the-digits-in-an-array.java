c53c79a2cf488271609a8f85b129e190f639ac5d
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-05-15 22:48:22 +0530

    Time: 6 ms (50.91%), Space: 46.6 MB (37.56%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -2,29 +2,30 @@ class Solution {
     public int[] separateDigits(int[] nums) {
         ArrayList<Integer> ans = new ArrayList<>();
 
-        // for(int i=0; i<nums.length;i++)
-        // {
-        //     String s = String.valueOf(nums[i]);
-        //     for(int j=0; j<s.length(); j++)
-        //     {
-        //         ans.add(s.charAt(j)-'0');
-        //     }
-        // }
-
-        for(int num : nums)
+        for(int i=0; i<nums.length;i++)
         {
-            Stack<Integer> s = new Stack<>();
-            while(num > 0)
+            String s = String.valueOf(nums[i]);
+            for(int j=0; j<s.length(); j++)
             {
-                s.push(num % 10);
-                num /= 10;
-            }
-
-            while(!s.isEmpty())
-            {
-                ans.add(s.pop());
+                ans.add(s.charAt(j)-'0');
             }
         }
+
+        // more space
+        // for(int num : nums)
+        // {
+        //     Stack<Integer> s = new Stack<>();
+        //     while(num > 0)
+        //     {
+        //         s.push(num % 10);
+        //         num /= 10;
+        //     }
+
+        //     while(!s.isEmpty())
+        //     {
+        //         ans.add(s.pop());
+        //     }
+        // }
 
         int [] arr = new int[ans.size()];
         for(int i=0; i<ans.size(); i++)
