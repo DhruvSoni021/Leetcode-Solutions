eae05fc16db25318f98e782543d81edd7a96b053
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-05-28 19:42:05 +0530

    Time: 2 ms (81.44%), Space: 42.4 MB (9.9%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,13 +1,35 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int i=1;
-        int sum = 0;
-        while(i<num)
-        {
-            if(num % i == 0) sum += i;
-            i++;
+        // my solution
+        
+        // int i=1;
+        // int sum = 0;
+        // while(i<num)
+        // {
+        //     if(num % i == 0) sum += i;
+        //     i++;
+        // }
+        // if(sum == num) return true;
+        // else return false;
+
+
+        // optimal 
+          if (num <= 1) return false;
+
+        int sum = 1;
+
+        for (int i = 2; i * i <= num; i++) {
+
+            if (num % i == 0) {
+
+                sum += i;
+
+                if (i != num / i) {
+                    sum += num / i;
+                }
+            }
         }
-        if(sum == num) return true;
-        else return false;
+
+        return sum == num;
     }
 }
