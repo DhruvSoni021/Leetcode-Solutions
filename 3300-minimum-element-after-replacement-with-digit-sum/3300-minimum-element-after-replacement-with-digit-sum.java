daea37f75797b621b1ec16f0940ef2e3cb6d7c5d
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-05-29 16:55:28 +0530

    Time: 1 ms (100%), Space: 44.7 MB (70.43%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
@@ -1,18 +1,37 @@
 class Solution {
     public int minElement(int[] nums) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
-        {
-            int r = 0;
-            int rem = 0;
-           while(nums[i]!=0)
-           {
-            r = nums[i] % 10;
-            rem += r;
-            nums[i] /= 10;
-           }
-           arr.add(rem); 
+    //     ArrayList<Integer> arr = new ArrayList<>();
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         int r = 0;
+    //         int rem = 0;
+    //        while(nums[i]!=0)
+    //        {
+    //         r = nums[i] % 10;
+    //         rem += r;
+    //         nums[i] /= 10;
+    //        }
+    //        arr.add(rem); 
+    //     }
+    //    return Collections.min(arr);
+
+
+    // optimal 
+          int min = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            int sum = 0;
+            int temp = num;
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            min = Math.min(min, sum);
         }
-       return Collections.min(arr);
+
+        return min;
     }
 }
