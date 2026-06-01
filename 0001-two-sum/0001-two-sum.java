6d1d1decf5c801af0e9d10f4cdb8ef9710019a67
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-06-01 11:56:57 +0530

    Time: 2 ms (99.38%), Space: 47.2 MB (39.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,30 +1,35 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        HashMap<Integer,Integer> m = new HashMap<>();
+        
         for(int i=0;i<nums.length;i++)
         {
-            for(int j=i+1;j<nums.length;j++)
+            int partner = target - nums[i];
+
+            if(m.containsKey(partner))
             {
-                if(nums[i]+nums[j]==target)
-                {
-                    return new int[]{i,j};
-                }
-            }
+                return new int[]{m.get(partner),i};
+            } 
+            m.put(nums[i],i);
         }
-        return new int[]{-1, -1};
+        return new int[]{};
 
 
 
-// not applicable as Arrays.srt changes the index of the current element and then at the time of returning the array index the indices are returned wrong 
-        // Arrays.sort(nums);
-        // int i=0,j=nums.length-1;
-        // while(i<j)
+
+        // Time Complexity = O(n^2)
+        // for(int i=0;i<nums.length;i++)
         // {
-        //     if(nums[i]+nums[j]==target) return new int[]{i,j};
-        //     else if(nums[i]+nums[j]>target) j--;
-        //     else if(nums[i]+nums[j]<target) i++;
+        //     for(int j=i+1;j<nums.length;j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //         {
+        //             return new int[]{i,j};
+        //         }
+        //     }
         // }
-        // return new int[]{-1,-1};
+        // return new int[]{-1, -1};
+
     }
 
 
