9333c6fa48fbea80812cfdd42070f6bbbf6a5549
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-06-12 18:35:47 +0530

    Time: 7 ms (73.53%), Space: 49.3 MB (91.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,13 +1,10 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
 
-          HashMap<String, List<String>> map = new HashMap<>();
-
-        for (String s : strs) {
-
+        for(String s : strs) 
+        {
             char[] arr = s.toCharArray();
-
-            // Bubble Sort
             for (int i = 0; i < arr.length - 1; i++) {
                 for (int j = 0; j < arr.length - i - 1; j++) {
                     if (arr[j] > arr[j + 1]) {
@@ -20,7 +17,8 @@ class Solution {
 
             String key = new String(arr);
 
-            if (!map.containsKey(key)) {
+            if(!map.containsKey(key)) 
+            {
                 map.put(key, new ArrayList<>());
             }
 
@@ -28,28 +26,5 @@ class Solution {
         }
 
         return new ArrayList<>(map.values());
-
-        // HashMap<String,ArrayList<String>> map = new HashMap<>();
-
-        // for(String s:strs)
-        // {
-        //     int[] freq = new int[26];
-        //     for(int i=0;i<s.length();i++)
-        //     {
-        //         freq[s.charAt(i)-'a']++;
-        //     }
-
-        //     String key = "";
-        //     for(int i=0;i<26;i++)
-        //     {
-        //         key += "*"+freq[i];
-        //     }
-        //     if(!map.containsKey(key))
-        //     {
-        //         map.put(key,new ArrayList<>());
-        //     }
-        //     map.get(key).add(s);
-        // }
-        // return new ArrayList<>(map.values());
     }
 }
