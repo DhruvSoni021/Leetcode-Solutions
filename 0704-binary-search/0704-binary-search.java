c2409ee4c175035694fe73a7c765b9788564062d
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-07-04 02:22:29 +0530

    Time: 0 ms (100%), Space: 48.3 MB (68.67%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,24 +1,37 @@
 class Solution {
+    public int helper(int[] nums,int target, int lo,int hi)
+    {   
+        if(lo>hi) return -1;
+        int mid = lo+(hi-lo)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target) return helper(nums,target,lo,mid-1);
+        else return helper(nums,target,mid+1,hi);
+    }
     public int search(int[] nums, int target) {
-        int low=0;
-        int high= nums.length-1;
+        int n = nums.length;
+        return helper(nums,target,0,n-1);
+
+
+
+        // int low=0;
+        // int high= nums.length-1;
   
-        while(low<=high)
-        {
-           int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            else if(target<nums[mid])
-            {
-                high = mid -1;
-            }
-            else
-            {
-                low = mid +1;
-            }
-        }
-        return -1;
+        // while(low<=high)
+        // {
+        //    int mid=low+(high-low)/2;
+        //     if(nums[mid]==target)
+        //     {
+        //         return mid;
+        //     }
+        //     else if(target<nums[mid])
+        //     {
+        //         high = mid -1;
+        //     }
+        //     else
+        //     {
+        //         low = mid +1;
+        //     }
+        // }
+        // return -1;
     }
 }
