20bc877fdc030aa9b518fab4c60db19938c8b8c1
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-07-06 18:46:18 +0530

    Time: 0 ms (100%), Space: 45.1 MB (31.53%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,19 +13,34 @@
  *     }
  * }
  */
+
+
 class Solution {
-    int target;
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        target = targetSum;
-        return helper(root,0);
-    }
-    public boolean helper(TreeNode root, int sum)
-    {
         if(root == null) return false;
-        sum+=root.val;
-        if(root.left==null && root.right==null) return sum == target;
-        boolean lsum = helper(root.left,sum);
-        boolean rsum = helper(root.right,sum);
-        return lsum||rsum;
+        if(root.left==null && root.right==null)
+        {
+            if(targetSum==root.val) return true;
+            else return false;
+        }
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
     }
 }
+
+
+// class Solution {
+//     int target;
+//     public boolean hasPathSum(TreeNode root, int targetSum) {
+//         target = targetSum;
+//         return helper(root,0);
+//     }
+//     public boolean helper(TreeNode root, int sum)
+//     {
+//         if(root == null) return false;
+//         sum+=root.val;
+//         if(root.left==null && root.right==null) return sum == target;
+//         boolean lsum = helper(root.left,sum);
+//         boolean rsum = helper(root.right,sum);
+//         return lsum||rsum;
+//     }
+// }
