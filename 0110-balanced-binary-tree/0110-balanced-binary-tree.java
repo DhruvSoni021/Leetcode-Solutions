ffb5b8c1b5e445cb0211ef22c81fe544527ed0dc
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-07-11 23:52:28 +0530

    Time: 0 ms (100%), Space: 45.5 MB (76.94%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,16 +14,32 @@
  * }
  */
 class Solution {
+    static boolean flag ;
     public boolean isBalanced(TreeNode root) {
         if(root==null) return true;
-        int leftlevel = levels(root.left);
-        int rightlevel = levels(root.right);
-        if(Math.abs(leftlevel-rightlevel)>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        flag = true;
+        levels(root);
+        return flag;
     }
     public int levels(TreeNode root)
     {
         if(root==null) return 0;
-        return 1+Math.max(levels(root.left),levels(root.right));
+        int leftlevel = levels(root.left);
+        int rightlevel = levels(root.right);
+        if(Math.abs(leftlevel-rightlevel)>1) flag = false;
+        return 1+Math.max(leftlevel,rightlevel);
     }
+
+    // public boolean isBalanced(TreeNode root) {
+    //     if(root==null) return true;
+    //     int leftlevel = levels(root.left);
+    //     int rightlevel = levels(root.right);
+    //     if(Math.abs(leftlevel-rightlevel)>1) return false;
+    //     return isBalanced(root.left) && isBalanced(root.right);
+    // }
+    // public int levels(TreeNode root)
+    // {
+    //     if(root==null) return 0;
+    //     return 1+Math.max(levels(root.left),levels(root.right));
+    // }
 }
