e743bb73356aac22acf07da6c30bc90029e69909
Dhruv Soni <dhruvsoni0721@gmail.com>
2026-07-22 17:55:02 +0530

    Time: 2 ms (98.48%), Space: 46.4 MB (88.34%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,44 +1,68 @@
 class Solution {
-    public int[] leftRightDifference(int[] nums) {
-        int n = nums.length;
-        int[] leftSum = new int[n];
-        int[] rightSum = new int[n];
-        for(int i = n-1;i>=0;i--)
-        {   
-            if(i==0) leftSum[i] = 0;
-            else 
-            {
-                int sum=0;
-                for(int j = i-1;j>=0;j--)
-                {
-                    sum += nums[j];
-                }
-                leftSum[i]=sum;
-            }
-        }
-        for(int i=0;i<n;i++)
+    // total sum = leftsum + nums[i] + rightsum;
+
+     public int[] leftRightDifference(int[] nums) {
+        int total = 0;
+        for(int num : nums) 
         {
-            if(i==n-1) rightSum[i]=0;
-            else
-            {
-                int sum=0;
-                for(int j=i+1;j<n;j++)
-                {
-                    sum+=nums[j];
-                }
-                rightSum[i]=sum;
-            }
+            total+=num;
         }
-        int [] arr = new int[n];
-        for(int i=0;i<n;i++)
+        int leftSum = 0;
+        int [] arr = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
         {
-            arr[i] = abs(leftSum[i],rightSum[i]);
+            total-=nums[i];                  // now it is right sum as total
+            arr[i] = Math.abs(leftSum - total);
+            leftSum+=nums[i];
         }
         return arr;
-    }
-    public int abs(int left,int right)
-    {
-        if(left-right < 0) return -(left-right);
-        else return left-right;
-    }
+     }
+
+
+
+
+
+     // TC -> O(n^2)
+    // public int[] leftRightDifference(int[] nums) {
+    //     int n = nums.length;
+    //     int[] leftSum = new int[n];
+    //     int[] rightSum = new int[n];
+    //     for(int i = n-1;i>=0;i--)
+    //     {   
+    //         if(i==0) leftSum[i] = 0;
+    //         else 
+    //         {
+    //             int sum=0;
+    //             for(int j = i-1;j>=0;j--)
+    //             {
+    //                 sum += nums[j];
+    //             }
+    //             leftSum[i]=sum;
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(i==n-1) rightSum[i]=0;
+    //         else
+    //         {
+    //             int sum=0;
+    //             for(int j=i+1;j<n;j++)
+    //             {
+    //                 sum+=nums[j];
+    //             }
+    //             rightSum[i]=sum;
+    //         }
+    //     }
+    //     int [] arr = new int[n];
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         arr[i] = abs(leftSum[i],rightSum[i]);
+    //     }
+    //     return arr;
+    // }
+    // public int abs(int left,int right)
+    // {
+    //     if(left-right < 0) return -(left-right);
+    //     else return left-right;
+    // }
 }
